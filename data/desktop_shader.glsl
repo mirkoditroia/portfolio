ceb8309fcc35ef19273526ff926c972e6853b56b
mirkoditroia <mirkoditroia@gmail.com>
2025-07-16 23:31:20 +0200

    css bug fix

--- data/desktop_shader.glsl
+++ data/desktop_shader.glsl
@@ -9,31 +9,20 @@ uniform vec2 u_resolution;
 #define iResolution u_resolution
 
 void main() {
-    vec2 I = gl_FragCoord.xy;
-    float t = iTime;
-    float z = 0.0, d = 0.0, s = 0.0;
-    vec4 O = vec4(0.0);
+   for (int j = 1; j <= 9; j++) {
+    float i = float(j) * 0.1;
 
-    // Usa int per il loop principale
-    for (int j = 0; j < 100; j++) {
-        float i = float(j);
-        vec3 p = z * normalize(vec3(I + I, 0.0) - iResolution.xyy);
-        p.z -= t;
+    u = normalize(vec3(uv, 0.7 * sqrt(i))) * s;
+    u.yz *= pitch;
+    u.xz *= yaw;
 
-        d = 1.0;
-        for (int k = 0; k < 6; k++) {
-            if (d >= 64.0) break;
-            p += 0.7 * cos(p.yzx * d) / d;
-            d += d;
-        }
+    a = mod(u, o) * 2.0 - o;
+    b = mod(u + o * 0.5, o) * 2.0 - o;
+    h = min(dot(a, a), dot(b, b)) * 0.5;
 
-        p.xy *= mat2(cos(z * 0.2 + vec4(0, 11, 33, 0)));
-        z += d = 0.03 + 0.1 * max(s = 3.0 - abs(p.x), -s * 0.2);
-        O += (cos(s + s - vec4(5, 0, 1, 3)) + 1.4) / d / z;
-    }
+    v = h * h * H(i - t / 5.0);
+    k = 1.1 - max(i, tr - i);
+    c += v * k;
+}
 
-    // Approx tanh
-    vec4 result = (exp(2.0 * (O * O / 400000.0)) - 1.0) / (exp(2.0 * (O * O / 400000.0)) + 1.0);
-
-    gl_FragColor = result;
 }
--- data/mobile_shader.glsl
+++ data/mobile_shader.glsl
@@ -4,46 +4,65 @@ precision mediump float;
 
 uniform float u_time;
 uniform vec2 u_resolution;
+uniform vec4 u_mouse;
 
 #define iTime u_time
 #define iResolution u_resolution
+#define iMouse u_mouse
+
+#define H(a) (cos(radians(vec3(100.0, 140.0, 190.0)) - ((a) * 6.2832)) * 0.5 + 0.5)
+
+mat2 rot(float a) {
+    float c = cos(a);
+    float s = sin(a);
+    return mat2(c, -s, s, c);
+}
 
 void main() {
-    vec2 I = gl_FragCoord.xy;
-    float t = iTime;
-    float z = 0.0, d = 0.0, s = 0.0;
-    vec4 O = vec4(0.0);
+    vec2 U = gl_FragCoord.xy;
+    vec3 c = vec3(0.0), o = vec3(1.0, 1.732, 1.0), u, v, a, b;
+    vec2 R = iResolution.xy;
+    vec2 m = iMouse.xy / R * 4.0 - 2.0;
+    vec2 uv = (U - 0.5 * R) / R.y;
+    float t = iTime / 5.0;
+    float tr = smoothstep(0.0, 1.0, sin(t) * 0.6 + 0.5);
+    float s = tr * 4.0 + 2.0;
+    float h, k, i;
+    if (iMouse.z < 1.0) m = vec2(sin(t / 2.0) * 0.6, sin(t) * 0.4);
 
-    // Usa int per il loop principale
-    for (int j = 0; j < 100; j++) {
-        float i = float(j);
-        vec3 p = z * normalize(vec3(I + I, 0.0) - iResolution.xyy);
-        p.z -= t;
+    mat2 pitch = rot(m.y * 1.571);
+    mat2 yaw = rot(m.x * 1.571);
 
-        d = 1.0;
-        for (int k = 0; k < 6; k++) {
-            if (d >= 64.0) break;
-            p += 0.7 * cos(p.yzx * d) / d;
-            d += d;
-        }
+    for (int j = 1; j <= 9; j++) {
+        float i = float(j) * 0.1;
 
-        p.xy *= mat2(cos(z * 0.2 + vec4(0, 11, 33, 0)));
-        z += d = 0.03 + 0.1 * max(s = 3.0 - abs(p.x), -s * 0.2);
-        O += (cos(s + s - vec4(5, 0, 1, 3)) + 1.4) / d / z;
+        u = normalize(vec3(uv, 0.7 * sqrt(i))) * s;
+        u.yz *= pitch;
+        u.xz *= yaw;
+
+        a = mod(u, o) * 2.0 - o;
+        b = mod(u + o * 0.5, o) * 2.0 - o;
+        h = min(dot(a, a), dot(b, b)) * 0.5;
+
+        v = h * h * H(i - t / 5.0);
+        k = 1.1 - max(i, tr - i);
+        c += v * k;
     }
 
-    // Approx tanh
-    vec4 color = (exp(2.0 * (O * O / 400000.0)) - 1.0) / (exp(2.0 * (O * O / 400000.0)) + 1.0);
+    vec3 color = pow(c, vec3(2.5)) * 0.5 + 0.05;
 
-    // Vignettatura
-    vec2 uv = I / iResolution.xy * 2.0 - 1.0;
-    vec2 oriuv = uv;
-    uv.y *= iResolution.y / iResolution.x;
+    // Calcola coordinate normalizzate schermo
+    vec2 screenUV = U / iResolution.xy;
+
+    // Bordo sfumato
+    float borderSize = 0.05; // dimensione bordo sfumato
+    float fadeX = smoothstep(0.0, borderSize, screenUV.x) * smoothstep(1.0, 1.0 - borderSize, screenUV.x);
+    float fadeY = smoothstep(0.0, borderSize, screenUV.y) * smoothstep(1.0, 1.0 - borderSize, screenUV.y);
+    float borderFade = fadeX * fadeY;
 
     vec3 borderColor = vec3(20.0 / 255.0, 30.0 / 255.0, 48.0 / 255.0);
-    float vignette = pow(max(0.0, 0.95 - length(oriuv * oriuv * oriuv * vec2(1.05, 1.1))), 0.3);
 
-    vec3 finalColor = mix(borderColor, color.rgb, vignette);
+    vec3 finalColor = mix(borderColor, color, borderFade);
 
     gl_FragColor = vec4(finalColor, 1.0);
 }
